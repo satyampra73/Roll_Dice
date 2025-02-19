fedd48e6cbb4f8f2f030481e204e63fc17043ab4
Satyam Prajapati <satyam7355@gmail.com>
2025-02-19 19:47:58 +0530

    git ignore updated

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     const MaterialApp(
+      debugShowCheckedModeBanner: false,
     home: Scaffold(
       body: Center(
       child: Text("Roll Dice"),
