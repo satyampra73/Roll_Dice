f3719b8339f8edde530ca2c57ed8d163188374f2
Satyam Prajapati <satyam7355@gmail.com>
2025-02-19 21:39:53 +0530

    gradient added

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,24 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       debugShowCheckedModeBanner: false,
     home: Scaffold(
-      body: Center(
-      child: Text("Roll Dice"),
+      backgroundColor: Color.fromARGB(255, 11, 197, 135),
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              const Color.fromARGB(255, 103, 28, 126),
+              const Color.fromARGB(255, 55, 131, 32)
+            ],
+            begin: Alignment.topLeft,
+            end:Alignment.bottomRight
+            )
+        ),
+        child: const Center(
+        child: Text("Roll Dice"),
+        ),
       ),
       ),
     ),
