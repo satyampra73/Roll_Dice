7c7ab51a40d0aaebad4299d937d34cb30d653a27
Satyam Prajapati <satyam7355pra@gmail.com>
2025-02-21 17:55:17 +0530

    working on adding veraibles

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,29 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:rolldice/style_text.dart';
 
-class GradientContainer extends StatelessWidget{
+var startAlignment = Alignment.topLeft;
+var endAlignment = Alignment.bottomRight;
 
+class GradientContainer extends StatelessWidget {
   const GradientContainer({super.key});
-  
+
   @override
   Widget build(context) {
     return Container(
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            colors: [
-              const Color.fromARGB(255, 103, 28, 126),
-              const Color.fromARGB(255, 55, 131, 32)
-            ],
-            begin: Alignment.topLeft,
-            end:Alignment.bottomRight
-            )
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+           colors: [
+             Color.fromARGB(255, 103, 28, 126),
+             Color.fromARGB(255, 55, 131, 32),
+          ],
+          begin: startAlignment,
+          end: endAlignment,
         ),
-        child: const Center(
-        child: Text("Roll Dice",style: TextStyle(
-          color: Colors.white,
-          fontSize: 24.0
-        ),),
-        ),
-      );
+      ),
+      child: const Center(child: StyledText()),
+    );
   }
-
 }
---
+++ lib/style_text.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class StyledText extends StatelessWidget {
+  const StyledText({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      "Roll Dice",
+      style: TextStyle(color: Colors.white, fontSize: 24.0),
+    );
+  }
+}
